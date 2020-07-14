e2d240d3e68951c2ab197376f30e28b277b4080c
Manuel Dillmann <manuel-dillmann@gmx.de>
2020-07-15 00:01:51 +0200

    startet implement an object oriented controller ui

---
+++ Controller/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 23:08:09 2020
+
+@author: Manuel
+"""
+
+import classes as c
+import tkinter as tk
+
+
+
+if __name__ == "__main__":
+    # root = tk.Tk()
+    app = c.myApp(tk.Tk())
+    app.mainloop()
